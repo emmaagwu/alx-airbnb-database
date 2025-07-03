4cd9fc3d05e64982934590345c4ec0cef79b9c4c
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-03 12:09:20 +0100

    Change ranking function from RANK() to ROW_NUMBER() in SQL script for booking properties

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,13 +10,13 @@ GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
 
--- 2. Ranking properties by number of bookings using RANK()
+-- 2. Ranking properties by number of bookings using ROW_NUMBER()
 
 SELECT 
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
