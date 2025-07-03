7b0b046ae434ca0c9f9b28bf9e5185421d2afc2c
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-03 12:36:50 +0100

    Optimize performance analysis query by adding filters for confirmed bookings and start date.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,18 +1,20 @@
--- Original (Unoptimized) Query
+-- Performance analysis for original (unoptimized) query
+
+EXPLAIN
 SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
     b.total_price,
     b.status,
-    
+
     u.first_name,
     u.last_name,
     u.email,
-    
+
     p.name AS property_name,
     p.location,
-    
+
     pay.amount,
     pay.payment_method,
     pay.payment_date
@@ -20,4 +22,5 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+LEFT JOIN Payment pay ON pay.booking_id = b.booking_id
+WHERE b.status = 'confirmed' AND b.start_date >= '2024-01-01';
